ac36f53bc4b1d1510466df75a784c7db08613ffa
Mauricio AB <dev.mauricioalezandre@gmail.com>
2026-07-26 18:53:20 -0300

    test: packages/domain change (both API and Web should run)

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -3,3 +3,4 @@ export * from "./entities/risk-signal";
 export * from "./entities/consent-record";
 export * from "./entities/crisis-session";
 export * from "./entities/chat-message";
+// ci-verify Sun Jul 26 18:53:19 ESAST 2026
